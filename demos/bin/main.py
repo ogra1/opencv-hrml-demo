47151ff13ae765efba1a26fa643c78f4f8a1903b
Oliver Grawert <ogra@ubuntu.com>
2022-11-22 12:58:20 +0100

    make flask server listen on all IPs and port 6063

--- demos/bin/main.py
+++ demos/bin/main.py
@@ -19,5 +19,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='0.0.0.0', port=6063, debug=True)
     #app.run(host='192.168.2.94', debug=True)
